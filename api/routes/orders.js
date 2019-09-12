8bad1b93cc4f992531fe44682a6f3c18035403f3
mac <>
2019-09-12 18:06:44 +0700

    update 1

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,9 +8,30 @@ router.get('/', (req, res, next)=>{
 });
 
 router.post('/', (req, res, next)=>{
-    res.status(200).json({
-        message:'Oder was created!'
+
+    const order ={ 
+        productId: req.body.productId,
+        quantily: req.body.quantily
+    };
+
+    res.status(201).json({
+        message:'Oder was created!',
+        order: order
     });
 });
+
+router.get('/:orderId',(req, res, next)=>{
+    res.status(200).json({
+        message:'order details',
+        orderId: req.params.orderId
+    });
+});
+
+router.delete('/:orderId',(req, res, next)=>{
+    res.status(200).json({
+        message:'delete details',
+        orderId: req.params.orderId
+    });
+}); 
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,15 @@ router.get('/', (req,res, next)=>{
 });
 
 router.post('/', (req, res, next)=>{
-    res.status(200).json({
-        message:'Handling POST request to /products'
+
+    const product ={ 
+        name: req.body.name,
+        price :  req.body.price
+    };
+
+    res.status(201).json({
+        message:'Handling POST request to /products',
+        createdProduct: product
     });;
 });
 
